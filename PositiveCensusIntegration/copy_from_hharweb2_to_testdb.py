15c09d046ea75bc499b01f6f347ff20e70bf8da7
Frederick Sells <frederick.sells@hebrewhome.org>
2020-04-02 14:06:00 -0400

    fixed copy for dev odbc

--- PositiveCensusIntegration/copy_from_hharweb2_to_testdb.py
+++ PositiveCensusIntegration/copy_from_hharweb2_to_testdb.py
@@ -2,29 +2,36 @@ import pyodbc
 import datetime
 DEBUG = True
 
-HHARWEB2 = (r'DRIVER={SQL Server};'
-            r'SERVER=HHARWEB2\SQLEXPRESS;'
-            r'DATABASE=PositiveCensusReport;'
-            r'Trusted_Connection=yes;'    )
+HHARWEB2 = (r'DSN=copybedchecks32;'
+            r'UID=copybedchecks;'
+            r'PWD=RepresentativeDreamAdmireFaint2;'  )
 
 POSITIVE_CENSUS_TEST = (r'DRIVER={SQL Server};'
             r'SERVER=HHARWEB2\SQLEXPRESS;'
-            r'DATABASE=PositiveCensus_Test;'
+            r'DATABASE=PositiveCensus;'
             r'Trusted_Connection=yes;'    )
 
 
-LOCAL_HHdev = (r'DRIVER={SQL Server};'
+LAPTOP = (r'DRIVER={SQL Server};'
                 r'SERVER=.;'
                #HHSWLSQLDEV01
-                r'DATABASE=HHdev;'
+                r'DATABASE=FredTesting;'
                 r'Trusted_Connection=yes;'    )
 
-def get_old_data(conn_str):
-    Connection = pyodbc.connect(conn_str)
+HHSWLDEV02 = (  #this works, even though UID and PWD are defined in ODBC DSN 32 bit
+    r'DSN=censusapps32;'
+    r'UID=hhcensus;'
+    r'PWD=Plan-Tree-Scale-Model-Seed-9;'
+    )  
+
+
+def get_old_data(Connection):
+    #Connection = pyodbc.connect(conn_str)
     Cursor = Connection.cursor()
-    Cursor.execute('SET NOCOUNT ON')
-    Connection.commit()
-    sql = '''SELECT pcr.Unit, Room, ResidentNumber, ResidentName, Status, LevelOfCare,Gender
+    #Cursor.execute('SET NOCOUNT ON')
+    #Connection.commit()
+    sql = '''
+             SELECT pcr.Unit, Room, ResidentNumber, ResidentName, Status, LevelOfCare,Gender
                       ,OrigAdmitDate,  YesNo, Reason, RepDate, Comments
                       , pcr.update_by, update_dt, pcr.create_dt, users.UserName
                 FROM [dbo].[PositiveCensusReport] AS pcr
@@ -32,8 +39,8 @@ def get_old_data(conn_str):
                     ON pcr.update_by=logins.PositiveCensusReportLoginID
                 LEFT JOIN [dbo].[PositiveCensusReportUsers] AS users 
                     ON logins.PositiveCensusReportUserID=users.PositiveCensusReportUserID
-                WHERE pcr.RepDate >= '2019-11-15' AND pcr.RepDate <= '2019-11-28'
-                ORDER BY pcr.RepDate, Unit, Room
+                WHERE pcr.RepDate >= '2020-03-29' AND pcr.RepDate <= '2020-04-02'
+                ORDER BY pcr.RepDate, Unit, Room;
         '''
     Cursor.execute(sql)
     rows = Cursor.fetchall()
@@ -41,16 +48,16 @@ def get_old_data(conn_str):
     Connection.close()
     return rows
 
-def insert_data(conn_str, data):
-    Connection = pyodbc.connect(conn_str)
+def insert_data(Connection, data):
+    #Connection = pyodbc.connect(conn_str)
     Cursor = Connection.cursor()
     Cursor.execute('SET NOCOUNT ON')
     #Cursor.execute('SET IDENTITY_INSERT dbo.PositiveCensusReport  ON')
     Connection.commit()
-    sql = '''INSERT  dbo.PositiveCensusReport
+    sql = '''INSERT  dbo.NightlyBedCheck
                   (Unit ,Room  ,ResidentNumber ,ResidentName
-                  ,Status  ,LevelOfCare  ,Gender  ,OrigAdmitDate
-                  ,YesNo  ,Reason ,RepDate   ,Comments
+                  ,Status  ,LevelOfCare  ,Gender  ,CurrentAdmitDate
+                  ,Inbed  ,Reason ,RepDate   ,Comments
                   ,UpdateByID  ,UpdateDatetime  ,CreateDatetime  ,UpdateByName
                     ,Obsolete)   
                  VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 1)
@@ -58,10 +65,20 @@ def insert_data(conn_str, data):
     Cursor.executemany(sql, data)
     Connection.commit()
     Cursor.close()
+    
+def execute(source, destination, DEBUG=False):
+    data = get_old_data(source)
+    for x in data[:9]: print('census', x)
+    print('nrecords', len(data))
+    if DEBUG:
+        print('DEBUG=True, no data written to DB')
+    else:
+        insert_data(destination, data)
+        print ('data inserted into target')
+    print ('done')
 
 if __name__ == '__main__':
-    data = get_old_data(HHARWEB2)
-    if DEBUG:
-        for x in data[:9]: print(len(x), x)
-    insert_data(LOCAL_HHdev, data)
-    print ('done')
+    source = pyodbc.connect(HHARWEB2)
+    target = pyodbc.connect(HHSWLDEV02)
+    execute(source, target, DEBUG=False)
+    
