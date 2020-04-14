58417cc506ef88e7c324b846fb4b68740152bc10
Frederick Sells <frederick.sells@hebrewhome.org>
2020-04-14 08:19:31 -0400

    send email at 2,4,6 am logic, scheduler to be done via windows scheduler

---
+++ webapp/management/commands/email_error_warnings.py
@@ -0,0 +1,69 @@
+'''
+Created on Feb 27, 2020
+The purpose of this module is to send emails to a specified distribution list
+for all residents where the inbed status is blank or is no and the reason is blank.
+
+@author: fsells
+'''
+
+import sys, os, datetime
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+from webapp.constants import *
+from webapp import logic_census as logic
+from webapp import email_sender
+
+HTML_WRAPPER = '''<meta http-equiv="Content-Type" content="text/html; charset=us-ascii">
+                    <h3>Incomplete/Error in Nightly Bed Check</h3>
+                    <table border="1"><tbody>
+                    {}
+                    </tbody></table>'''
+
+def TR(tds, htmlclass=None):
+    text = ' '.join(tds)
+    return '<tr>{}</tr>'.format(text or '')
+
+def TH(text):
+    return '<th>{}</th>'.format(text or '')
+
+def TD(text):
+    return '<td>{}</td>'.format(text or '')
+
+class Command(BaseCommand):
+    help = 'Sends Warnings of errors in NightlyBedcheck data'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--testonly', action= 'store_true', help = 'print info only, dont send', default=False)
+        
+    def format_email_body(self, errors):
+        header = 'Unit	RepDate	Room	ResidentName	Status	LevelOfCare	Gender	CurrentAdmitDate	Inbed	Reason	Comments'.split()
+        rows = [TR([TH(x) for x in header])]
+        for error in errors:
+            tr =TR( [TD(error[key]) for key in header] )
+            rows.append(tr)
+        for row in rows: print('\n', type(row), row)
+        return HTML_WRAPPER.format( '\n'.join(rows) )
+
+
+    def handle(self, *args, **options):
+        # print('options', options)
+        errors = logic.get_errors() 
+        now = datetime.datetime.now().strftime('%x %X')
+        if errors:
+            body=self.format_email_body(errors)
+            subject = '{} errors reported at {}'.format(len(errors),  now)
+        else:
+            body=HTML_WRAPPER.format('''<h1>Don't Worry, Be Happy there are no errors reported</H1>''')
+            subject = 'No errors reported at {}'.format( now )  
+        # print (subject)
+        # print(body)
+        # print('done')
+        email_sender.email_anything(subject, body)
+        print('done')
+        
+            
+            
+    
+        
